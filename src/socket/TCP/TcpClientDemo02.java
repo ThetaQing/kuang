c743c7fe53d8e588737cedd85514fd179bd4f3b6
ThetaQing <csusq2016@163.com>
2020-11-02 21:23:01 +0800

    三次握手——文件传输

--- src/socket/TCP/TcpClientDemo02.java
+++ src/socket/TCP/TcpClientDemo02.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
-import java.net.UnknownHostException;
 
 public class TcpClientDemo02 {
     public static void main(String[] args){
@@ -16,14 +15,14 @@ public class TcpClientDemo02 {
         FileInputStream fis = null;
         try {
             //1、创建一个socket连接
-            socket = new Socket(InetAddress.getByName("127.0.0.1"), 9999);
+            socket = new Socket(InetAddress.getByName("127.0.0.1"), 9000);
             // 2 create an output stream
             os = socket.getOutputStream();
 
             // 3. file stream
-            // 3.1 read in file
-            fis = new FileInputStream(new File("lychee.jpg"));
-            // 3.2 write out the file
+            // 3.1 读入文件
+            fis = new FileInputStream(new File("G://Code//Java//kuang//src//socket//TCP//lychee.jpg"));
+            // 3.2 将这个文件写出
             byte[] buffer = new byte[1024];
             int len;
             while((len = fis.read(buffer))!= -1){
---
+++ src/socket/TCP/TcpServerDemo02.java
@@ -0,0 +1,39 @@
+package socket.TCP;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TcpServerDemo02 {
+    public static void main(String[] args) {
+        try {
+
+            // 1 create a socket
+            ServerSocket serverSocket = new ServerSocket(9000);
+            //2. listen to client
+            Socket socket = serverSocket.accept();  // 阻塞式监听，会一直等待客户端连接
+            // 3. get instream
+            InputStream is = socket.getInputStream();  // 读取文件流
+
+            // 4. 文件输出 将这个文件写出
+            FileOutputStream fos = new FileOutputStream(new File("G://Code//Java//kuang//src//socket//TCP//receive.png"));
+
+            byte[] buffer = new byte[1024];
+            int len;
+            while((len = is.read(buffer))!= -1){
+                fos.write(buffer,0,len);
+            }
+            // 5. 关闭资源
+            fos.close();
+            is.close();
+            socket.close();
+            serverSocket.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
