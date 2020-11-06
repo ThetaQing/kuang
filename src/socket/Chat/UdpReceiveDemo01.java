2cd87f5d0eb86205bee57823ce55ebb747c2c580
ThetaQing <csusq2016@163.com>
2020-11-06 20:59:53 +0800

    Udp单方面循环实现发送和接收

--- src/socket/Chat/UdpReceiveDemo01.java
+++ src/socket/Chat/UdpReceiveDemo01.java
@@ -1,4 +1,46 @@
 package socket.Chat;
 
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
 public class UdpReceiveDemo01 {
+    public static void main(String[] args) {
+
+        try {
+            // 1、开启端口
+            DatagramSocket socket = new DatagramSocket(8080);
+            // 2、接收包
+
+            while(true){
+                byte[] msg = new byte[1024];  // 准备接收的包裹
+                DatagramPacket packet = new DatagramPacket(msg, 0, msg.length);
+
+
+                socket.receive(packet);  // 阻塞式接收包裹
+                // 3、显示并判断
+                byte[] data = packet.getData();
+                String receiveData = new String(data, 0, data.length);
+                System.out.println(receiveData);
+                if (receiveData.contains("bye")) {
+                    System.out.println("see you~");
+
+
+                    break;
+
+                }
+
+            }
+            System.out.println("end");
+            socket.close();
+
+
+
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
 }
--- src/socket/Chat/UdpSenderDemo01.java
+++ src/socket/Chat/UdpSenderDemo01.java
@@ -1,4 +1,39 @@
 package socket.Chat;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.*;
+
 public class UdpSenderDemo01 {
+    public static void main(String[] args) {
+        new Thread();
+
+
+        try {
+            // 1、 创建socket
+            DatagramSocket socket = new DatagramSocket(8888);
+            // 2、创建包
+            // 准备数据信息
+
+            byte[] buffer = new byte[1024];
+            // 从控制台读取
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            while(true){
+                String data = reader.readLine();
+                DatagramPacket packet = new DatagramPacket(data.getBytes(),0,data.getBytes().length,new InetSocketAddress("localhost",8080));
+                // 3、发送包
+                socket.send(packet);
+                if(data.contains("bye")){
+                    System.out.println("see you~");
+                    break;
+                }
+
+            }
+            // 4 close
+            socket.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
 }
